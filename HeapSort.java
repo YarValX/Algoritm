f3cdb13994f05ecba67c2c32c08c4d036fa8705d
YarValX <124351076+YarValX@users.noreply.github.com>
2023-06-15 10:08:33 +0500

    sorting

--- HeapSort.java
+++ HeapSort.java
@@ -1,42 +1,33 @@
 public class HeapSort {
     public void sort(int arr[]) {
         int n = arr.length;
-        // Build heap (rearrange array)
         for (int i = n / 2 - 1; i >= 0; i--)
             heapify(arr, n, i);
 
-        // One by one extract an element from heap
         for (int i = n - 1; i >= 0; i--) {
-            // Move current root to end
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
-
-            // Call max heapify on the reduced heap
             heapify(arr, i, 0);
         }
     }
 
     private void heapify(int[] arr, int n, int i) {
-        int largest = i; // Initialize largest as root
-        int left = 2 * i + 1; // Left child
-        int right = 2 * i + 2; // Right child
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
 
-        // If left child is larger than root
         if (left < n && arr[left] > arr[largest])
             largest = left;
 
-        // If right child is larger than largest so far
         if (right < n && arr[right] > arr[largest])
             largest = right;
 
-        // If largest is not root
         if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
             arr[largest] = swap;
 
-            // Recursively heapify the affected sub-tree
             heapify(arr, n, largest);
         }
     }
@@ -48,7 +39,6 @@ public class HeapSort {
         System.out.println();
     }
 
-    // Управляющая программа
     public static void main(String args[])
     {
         int arr[] = {12, 11, 13, 5, 6, 7};
